34aea50a9d630ea10eb16ae58434ac9e1294ab36
Lewis Reid <lreidwilliams@gmail.com>
2020-05-29 23:16:53 +1000

    particle effects for jumping and sliding!

--- Assets/Behaviours/States/JumpingState.cs
+++ Assets/Behaviours/States/JumpingState.cs
@@ -12,12 +12,17 @@ public class JumpingState : State
     private float _postJumpDelay, _wallTouchPadding = 0.5f;
     [SerializeField]
     private AnimationCurve _fallCurve;
+    [SerializeField]
+    private ParticleSystem _landingParticles;
 
     protected override void Enter()
     {
         float xDirection = Mathf.Sign(4 - transform.position.x);
         _rigidbody.AddForce(new Vector2(_jumpStrength.x * xDirection, _jumpStrength.y), ForceMode.VelocityChange);
         StartCoroutine(ApplyJumpForce(Time.time, xDirection));
+        _landingParticles.transform.localEulerAngles = Vector3.up * (xDirection > 0 ? 0 : 180);
+        _landingParticles.transform.localPosition = Vector3.right * 0.5f * -xDirection;
+        _landingParticles.Play();
     }
 
     private IEnumerator ApplyJumpForce (float startTime, float xDirection)
--- Assets/Behaviours/States/SlidingState.cs
+++ Assets/Behaviours/States/SlidingState.cs
@@ -7,14 +7,19 @@ public class SlidingState : State
     private Rigidbody _rigidbody;
     [SerializeField, Range(1f, 5f)]
     private float _slidingDrag = 3;
+    [SerializeField]
+    private ParticleSystem _slideParticles;
 
     protected override void Enter()
     {
+        _slideParticles.transform.localPosition = new Vector2(0.5f * Mathf.Sign(transform.position.x - 4), 0.5f);
+        _slideParticles.Play();
         _rigidbody.drag = _slidingDrag;
     }
 
     protected override void Exit()
     {
+        _slideParticles.Stop();
         _rigidbody.drag = 1;
     }
 
