a979ed3b46def6df27c1b33518a114d203b33a43
Lewis Reid <lreidwilliams@gmail.com>
2020-06-19 22:46:36 +1000

    worked shield and immunity into life behaviour

--- Assets/Behaviours/Life.cs
+++ Assets/Behaviours/Life.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using Values;
 
 [RequireComponent(typeof(StateMachine))]
 public class Life : MonoBehaviour
@@ -6,11 +8,32 @@ public class Life : MonoBehaviour
     [SerializeField]
     private StateMachine _stateMachine;
     [SerializeField]
-    private bool _protected = true;
+    private AnimationFlag _immunityStatus, _shieldStatus;
+    [SerializeField]
+    private float _immunityDuration = 3f;
+    [SerializeField]
+    private ParticleSystem _shieldDropEffect;
 
     public void Damage ()
     {
-        if(!_protected)
-            _stateMachine.Change("Die");
+        if(!_immunityStatus.Active)
+        {
+            if (_shieldStatus.Active)
+            {
+                StartCoroutine(Immunity());
+                _shieldStatus.Active = false;
+                _shieldDropEffect.Play();
+            } else {
+                _stateMachine.Change("Die");
+            }
+        }
+    }
+
+
+    private IEnumerator Immunity ()
+    {
+        _immunityStatus.Active = true;
+        yield return new WaitForSeconds(_immunityDuration);
+        _immunityStatus.Active = false;
     }
 }
---
+++ Assets/Models/Values/AnimationFlag.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Values
+{
+    [Serializable]
+    public class AnimationFlag
+    {
+        [SerializeField]
+        private Animator _animator;
+        [SerializeField]
+        private string _key;
+
+        private bool _value;
+
+        public bool Active
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+                _animator.SetBool(_key, value);
+            }
+        }
+    }
+}
