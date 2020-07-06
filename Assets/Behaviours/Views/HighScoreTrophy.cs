411a4d595d3c7be285fdc98f95c6184eccc8473c
Lewis Reid <lreidwilliams@gmail.com>
2020-07-06 22:34:08 +1000

    added janky menu screen

---
+++ Assets/Behaviours/Views/HighScoreTrophy.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Views;
+
+public class HighScoreTrophy : MonoBehaviour
+{
+    [SerializeField]
+    private string _key;
+    [SerializeField]
+    private ScoreLabel _scoreLabel;
+
+    public void Awake()
+    {
+        var bestScore = PlayerPrefs.GetInt(_key, 0);
+        if (bestScore <= 0)
+            Destroy(gameObject);
+        _scoreLabel.Render(bestScore);
+    }
+}
---
+++ Assets/Behaviours/Views/PlayButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using Views;
+
+public class PlayButton : MonoBehaviour
+{
+    [SerializeField]
+    private TransitionScreen _screen;
+
+    private bool _fired = false;
+
+    public void Play ()
+    {
+        if (_fired) return;
+        _fired = true;  
+        _screen.Reverse(LoadGame);
+    }
+
+    private void LoadGame ()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
