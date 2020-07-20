35549083c4755da8fa50c3babe4d14e8fcfa1a77
Lewis Reid <lreidwilliams@gmail.com>
2020-07-20 22:57:39 +1000

    added camera transition when loading the game

--- Assets/Behaviours/Views/GameStart.cs
+++ Assets/Behaviours/Views/GameStart.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using Views;
 
@@ -6,15 +7,45 @@ public class GameStart : MonoBehaviour
 {
     [SerializeField]
     private TransitionScreen _screen;
+    [SerializeField]
+    private Transform _cameraTransform;
+    [SerializeField]
+    private float _cameraScrollSpeed = 1f, _loadDuration = 3f, _scrollAccelDuration = 3f;
+    [SerializeField]
+    private AnimationCurve _scrollCurve;
+    [SerializeField]
+    private Animator _titleAnimator;
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
-            _screen.Reverse(LoadGame);
+            StartScrolling();
+    }
+
+    private void StartScrolling ()
+    {
+        _titleAnimator.SetBool("Visible", false);
+        StartCoroutine(LoadGameAfterDelay());
+        StartCoroutine(ScrollCamera(Time.time + _scrollAccelDuration));
     }
 
     private void LoadGame ()
     {
         SceneManager.LoadScene("Game");
+    }
+
+    private IEnumerator LoadGameAfterDelay ()
+    {
+        yield return new WaitForSeconds(_loadDuration);
+        _screen.Reverse(LoadGame);
+    }
+
+    private IEnumerator ScrollCamera (float fullSpeedTime)
+    {
+        while (true)
+        {
+            _cameraTransform.position += Vector3.down * _cameraScrollSpeed * _scrollCurve.Evaluate(Time.time / fullSpeedTime) * Time.deltaTime;
+            yield return null;
+        }
     }
 }
