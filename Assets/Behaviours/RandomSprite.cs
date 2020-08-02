5bd1fa6010b6eac4120b445fdd2e045b80c60d32
Lewis Reid <lreidwilliams@gmail.com>
2020-08-02 22:06:53 +1000

    added pipe background variation

---
+++ Assets/Behaviours/RandomSprite.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class RandomSprite : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer _renderer;
+    [SerializeField]
+    private Sprite[] _sprites;
+
+    public void Roll ()
+    {
+        _renderer.sprite = _sprites[Random.Range(0, _sprites.Length)];
+    }
+}
